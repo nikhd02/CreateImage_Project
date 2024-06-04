f6ddc101909ed474c5c7cf88466177d0193b01a5
nikhd02 <94749949+nikhd02@users.noreply.github.com>
2024-06-05 02:27:33 +0530

    Update HomePage.js

--- Day14/src/pages/HomePage/HomePage.js
+++ Day14/src/pages/HomePage/HomePage.js
@@ -1,14 +1,44 @@
-import React from 'react'
-import './HomePage.css'
-import Navbar from '../Navbar/Navbar'
+// import React from 'react'
+// import './HomePage.css'
+// import Navbar from '../Navbar/Navbar'
+
+// const HomePage = () => {
+//     return (
+//         <div>
+//             <Navbar page="home" />
+//             Homepage
+//         </div>
+//     )
+// }
+
+// export default HomePage
+
+import React from 'react';
+import './HomePage.css';
+import Navbar from '../Navbar/Navbar';
+// import Footer from '../Footer/Footer';
 
 const HomePage = () => {
-    return (
-        <div>
-            <Navbar page="home" />
-            Homepage
+  return (
+    <div>
+      <Navbar page="home" />
+      <div className="hero-section">
+        <div className='hero-inner'>
+            <h1>Welcome to our website!</h1>
+            <p>This is a sample homepage.</p>
+            <button>Learn More</button>
         </div>
-    )
-}
+      </div>
+      {/* <div className="features-section">
+        <h2>Our Features</h2>
+        <ul>
+          <li>Feature 1</li>
+          <li>Feature 2</li>
+          <li>Feature 3</li>
+        </ul>
+      </div> */}
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
