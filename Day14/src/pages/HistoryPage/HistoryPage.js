2a39ef91b0e78f311c5a6df051544281c2737fbf
nikhd02 <94749949+nikhd02@users.noreply.github.com>
2024-06-08 21:35:09 +0530

    Update HistoryPage.js

--- Day14/src/pages/HistoryPage/HistoryPage.js
+++ Day14/src/pages/HistoryPage/HistoryPage.js
@@ -1,57 +1,4 @@
-// // import React, { useEffect, useState } from 'react'
-// // import Navbar from '../Navbar/Navbar'
-// // import './HistoryPage.css'
-// // // import { Link } from 'react-router-dom'
-// // import HistoryCard from './HistoryCard'
 
-// // const HistoryPage = () => {
-// //     const [data, setData] = useState([])
-// //     const [search, setSearch] = useState('')
-// //     const getData = async () => {
-// //         try {
-// //             // const response = await fetch('https://dummyjson.com/products')
-// //             // const response = await fetch(`https://dummyjson.com/products/search?q=${search}`)
-// //             const response = await fetch(`http://localhost:1010/api/v1/images`, {
-// //                 method: 'GET',
-// //                 headers: {
-// //                     'Content-Type': 'application/json',
-// //                     "Authorization": "Bearer " + localStorage.getItem('authToken')
-// //                 },
-
-// //             })
-// //             const obj = await response.json()
-// //             // let products = obj.products
-// //             // let images = obj.images
-// //             // if (searchText)
-// //             //     products = products.filter(item => item.title.toLowerCase().startsWith(searchText.toLowerCase()))
-// //             setData(obj.data)
-// //         } catch (e) {
-// //             console.log(e)
-// //         }
-// //     }
-
-// //     useEffect(() => {
-// //         getData()
-// //         // getData(search)
-// //     }, [search])
-
-// //     return (
-// //         <div>
-// //             <Navbar page="history" />
-// //             <input type='text' onChange={(e) => setSearch(e.target.value)} />
-// //             <div className='history-main-container'>
-// //                 {
-// //                     data.map(item => {
-// //                         console.log(item)
-// //                         return <HistoryCard key={item._id} item={item} />
-// //                     })
-// //                 }
-// //             </div>
-// //         </div>
-// //     )
-// // }
-
-// // export default HistoryPage
 
 // import React, { useEffect, useState } from 'react';
 // import Navbar from '../Navbar/Navbar';
@@ -59,43 +6,50 @@
 // import HistoryCard from './HistoryCard';
 
 // const HistoryPage = () => {
-//   const [data, setData] = useState([]);
-//   const [search, setSearch] = useState('');
-//   const userId = localStorage.getItem('userId'); // assuming you store the user ID in local storage
+//     const [data, setData] = useState([]);
+//     const [search, setSearch] = useState('');
 
-//   const getData = async () => {
-//     try {
-//       const response = await fetch(`http://localhost:1010/api/v1/images/user/${userId}`, {
-//         method: 'GET',
-//         headers: {
-//           'Content-Type': 'application/json',
-//           Authorization: `Bearer ${localStorage.getItem('authToken')}`,
-//         },
-//       });
-//       const obj = await response.json();
-//       setData(obj.data);
-//     } catch (e) {
-//       console.log(e);
-//     }
-//   };
+//     const getData = async () => {
+//         try {
+//             const response = await fetch(`http://localhost:1010/api/v1/images`, {
+//                 method: 'GET',
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                     'Authorization': 'Bearer ' + localStorage.getItem('authToken')
+//                 },
+//             });
+//             const obj = await response.json();
+//             setData(obj.data);
+//         } catch (e) {
+//             console.log(e);
+//         }
+//     };
 
-//   useEffect(() => {
-//     getData();
-//   }, []);
+//     useEffect(() => {
+//         getData();
+//     }, [search]);
 
-//   return (
-//     <div>
-//       <Navbar page="history" />
-//       <input type="text" onChange={(e) => setSearch(e.target.value)} />
-//       <div className="history-main-container">
-//         {data.map((item) => {
-//           console.log(item);
-//           return <HistoryCard key={item._id} item={item} />;
-//         })}
-//       </div>
-//     </div>
-//   );
+//     return (
+//         <div>
+//             <Navbar page="history" />
+//             <div className='history-main-container'>
+//                 <div className='history-search-bar'>
+//                     <input
+//                         type='text'
+//                         placeholder='Search history...'
+//                         onChange={(e) => setSearch(e.target.value)}
+//                     />
+//                 </div>
+//                 <div className='history-items'>
+//                     {data.map(item => (
+//                         <HistoryCard key={item._id} item={item} />
+//                     ))}
+//                 </div>
+//             </div>
+//         </div>
+//     );
 // };
+
 
 // export default HistoryPage;
 
@@ -106,6 +60,7 @@ import HistoryCard from './HistoryCard';
 
 const HistoryPage = () => {
     const [data, setData] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
     const [search, setSearch] = useState('');
 
     const getData = async () => {
@@ -119,6 +74,7 @@ const HistoryPage = () => {
             });
             const obj = await response.json();
             setData(obj.data);
+            setFilteredData(obj.data); // Initialize filteredData with all data
         } catch (e) {
             console.log(e);
         }
@@ -126,7 +82,15 @@ const HistoryPage = () => {
 
     useEffect(() => {
         getData();
-    }, [search]);
+    }, []);
+
+    useEffect(() => {
+        setFilteredData(
+            data.filter(item => 
+                item.name.toLowerCase().includes(search.toLowerCase())
+            )
+        );
+    }, [search, data]);
 
     return (
         <div>
@@ -140,7 +104,7 @@ const HistoryPage = () => {
                     />
                 </div>
                 <div className='history-items'>
-                    {data.map(item => (
+                    {filteredData.map(item => (
                         <HistoryCard key={item._id} item={item} />
                     ))}
                 </div>
