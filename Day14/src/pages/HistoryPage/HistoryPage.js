03dda3a80d510553100afa67c09fdda7d21a49a1
nikhd02 <94749949+nikhd02@users.noreply.github.com>
2024-06-11 14:54:28 +0530

    Update HistoryPage.js

--- Day14/src/pages/HistoryPage/HistoryPage.js
+++ Day14/src/pages/HistoryPage/HistoryPage.js
@@ -1,15 +1,15 @@
 
-import React, { useEffect, useState } from 'react';
-import Navbar from '../Navbar/Navbar';
-import './HistoryPage.css';
-import HistoryCard from './HistoryCard';
-require('dotenv').config();
+// import React, { useEffect, useState } from 'react';
+// import Navbar from '../Navbar/Navbar';
+// import './HistoryPage.css';
+// import HistoryCard from './HistoryCard';
+// require('dotenv').config();
 
 
-function HistoryPage() {
-    const [data, setData] = useState([]);
-    const [filteredData, setFilteredData] = useState([]);
-    const [search, setSearch] = useState('');
+// function HistoryPage() {
+//     const [data, setData] = useState([]);
+//     const [filteredData, setFilteredData] = useState([]);
+//     const [search, setSearch] = useState('');
 
 //     const getData = async () => {
 //         try {
@@ -62,50 +62,55 @@ function HistoryPage() {
 
 // export default HistoryPage;
 
-const getData = async () => {
-    try {
-        const BACKEND_URL= process.env.BACKEND_URL
-        const response = await fetch(`${BACKEND_URL}/api/v1/images`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + localStorage.getItem('authToken')
-            },
-        });
-        const obj = await response.json();
-        setData(obj.data);
-        setFilteredData(obj.data); // Initialize filteredData with all data
-    } catch (e) {
-        console.log(e);
-    }
-};
+import React, { useEffect, useState } from 'react';
+import Navbar from '../Navbar/Navbar';
+import './HistoryPage.css';
+import HistoryCard from './HistoryCard';
+require('dotenv').config();
+const HistoryPage = () => {
+    const [data, setData] = useState([]);
+    const [search, setSearch] = useState('');
 
-useEffect(() => {
-    getData();
-}, []);
+    const getData = async () => {
+        try {
+            const BACKEND_URL = process.env.BACKEND_URL
+            const response = await fetch(`${BACKEND_URL}/api/v1/images`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + localStorage.getItem('authToken')
+                },
+            });
+            const obj = await response.json();
+            setData(obj.data);
+        } catch (e) {
+            console.log(e);
+        }
+    };
 
-useEffect(() => {
-    setFilteredData(
-        data.filter(item => item.name.toLowerCase().includes(search.toLowerCase())
-        )
-    );
-}, [search, data]);
+    useEffect(() => {
+        getData();
+    }, [search]);
 
-return (
-    <div>
-        <Navbar page="history" />
-        <div className='history-main-container'>
-            <div className='history-search-bar'>
-                <input
-                    type='text'
-                    placeholder='Search history...'
-                    onChange={(e) => setSearch(e.target.value)} />
-            </div>
-            <div className='history-items'>
-                {filteredData.map(item => (
-                    <HistoryCard key={item._id} item={item} />
-                ))}
+    return (
+        <div>
+            <Navbar page="history" />
+            <div className='history-main-container'>
+                <div className='history-search-bar'>
+                    <input
+                        type='text'
+                        placeholder='Search history...'
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                </div>
+                <div className='history-items'>
+                    {data.map(item => (
+                        <HistoryCard key={item._id} item={item} />
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-);}
+    );
+};
+
+export default HistoryPage;
